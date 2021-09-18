93e8f097a7bc820d3a9e99f5d3fb3685d538e3c0
guptabhumika-12 <68741670+guptabhumika-12@users.noreply.github.com>
2021-09-18 20:24:56 +0530

    fixed header

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -10,9 +10,9 @@ const Profile = () => {
                 <div>
                     <div><h3>Name</h3></div>
                     <div><h3>Email</h3></div>
-                    <div><h3>Name</h3></div>
-                    <div><h3>Name</h3></div>
-                    <div><h3>Name</h3></div>
+                    <div><h3>Contact</h3></div>
+                    <div><h3>Profession</h3></div>
+                    <div><h3></h3></div>
                 </div>
             </div>
         </div>
