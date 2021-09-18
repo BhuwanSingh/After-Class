c4bc6952aef7404165a1ebeeef48dc07b043ac4f
guptabhumika-12 <68741670+guptabhumika-12@users.noreply.github.com>
2021-09-18 22:19:42 +0530

    profile fetch

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
-const Profile = () => {
+const Profile = (loggedin) => {
+
+    
+    // if (loggedin) {
+    //     const about = async () => {
+	// 	        const res = await fetch(`user/${loggedin}`, {
+    //             method: "GET",
+    //         });
+    //         const data = await res.json();
+    //         console.log(data)
+           
+    //     };
+    // }
+    let list = [];
+    useEffect(() => {
+    (async () => {
+
+        const about = async () => {
+		        const res = await fetch(`user/${loggedin}`, {
+                method: "GET",
+            });
+            const data = await res.json();
+            console.log("got data "+data)
+            list.push(data);
+        };
+        about();
+    
+     
+    })();
+  }, [loggedin]);
+
     return (
-        <div>
+        <div style={{color:"white"}}>
             <div>
                 <div>
                     image
+                    <div><h3>Created  </h3> {list.created}</div>
                 </div>
                 <div>
-                    <div><h3>Name</h3></div>
+                    <div><h3>Name</h3>{list.username}</div>
                     <div><h3>Email</h3></div>
                     <div><h3>Contact</h3></div>
-                    <div><h3>Profession</h3></div>
-                    <div><h3></h3></div>
+                    <div><h3>Saved Post</h3></div>
+                    <div><h3>Experience </h3></div>
                 </div>
             </div>
         </div>
