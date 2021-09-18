9796aa85f737ffdf68953d4c7efcde03ccb48987
Shrishesh Pratik <shrisheshpratik@gmail.com>
2021-09-18 14:14:26 -0400

    user data fetch

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,8 +1,10 @@
-import React,{useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+let data
 
-const Profile = (loggedin) => {
+const Profile = ({loggedin}) => {
 
-    
+    const [d, setd] = useState({})
+
     // if (loggedin) {
     //     const about = async () => {
 	// 	        const res = await fetch(`user/${loggedin}`, {
@@ -13,37 +15,40 @@ const Profile = (loggedin) => {
            
     //     };
     // }
-    let list = [];
+
+
     useEffect(() => {
     (async () => {
 
         const about = async () => {
-		        const res = await fetch(`user/${loggedin}`, {
+		        const res = await fetch(`/user/${localStorage.getItem('email')}`, {
                 method: "GET",
             });
-            const data = await res.json();
-            console.log("got data "+data)
-            list.push(data);
+            data = await res.json();
+            // console.log("got data "+data)
+            console.log(data[0])
+            setd(data[0])
         };
+
         about();
-    
-     
+
     })();
-  }, [loggedin]);
+        console.log(localStorage.getItem('email'));
+  }, []);
 
     return (
         <div style={{color:"white"}}>
             <div>
                 <div>
                     image
-                    <div><h3>Created  </h3> {list.created}</div>
+                    <div><h3>Created  </h3> {d.created}</div>
                 </div>
                 <div>
-                    <div><h3>Name</h3>{list.username}</div>
-                    <div><h3>Email</h3></div>
-                    <div><h3>Contact</h3></div>
-                    <div><h3>Saved Post</h3></div>
-                    <div><h3>Experience </h3></div>
+                    <div><h3>Name</h3>{d.username}</div>
+                    <div><h3>Email</h3>{d.email}</div>
+                    <div><h3>Contact</h3>{d.phone}</div>
+                    {/*<div><h3>Saved Post</h3>d.s</div>*/}
+                    <div><h3>Experience </h3>{d.experience}</div>
                 </div>
             </div>
         </div>
