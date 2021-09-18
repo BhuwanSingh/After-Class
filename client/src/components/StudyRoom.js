82ec16bc325668d2e2f8e32dd785ec55b064e318
guptabhumika-12 <68741670+guptabhumika-12@users.noreply.github.com>
2021-09-18 21:11:31 +0530

    studyroom clipboard

--- client/src/components/StudyRoom.js
+++ client/src/components/StudyRoom.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
+<<<<<<< Updated upstream
 import "./StudyRoom.css";
+=======
+import "./styles.css"
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+>>>>>>> Stashed changes
 var crypto = require("crypto");
 
 const StudyRoom = () => {
     const [url, seturl] = useState("");
+    const [copied, setCopied] = useState(false);
     const createURL = () => {
         var id = crypto.randomBytes(20).toString('hex');
         seturl("localhost:3000/whiteboard/"+id)
@@ -16,8 +22,19 @@ const StudyRoom = () => {
                 url ?
                     (<div tt>
                         {url}
+                        <CopyToClipboard
+                            className="bb btn btn-primary"
+                            text={url}
+                         onCopy={() => {setCopied(true)}}>
+                            {copied ?(<button style={{backgroundColor:"green"}}>Copied</button>): (<button>Copy</button>) }
+                        </CopyToClipboard>
+                        
                     </div>)
+<<<<<<< Updated upstream
                     :(<h1 className="to">(No url generated)</h1>)
+=======
+                    :(null)
+>>>>>>> Stashed changes
             }
         </div>
     )
