9bebf283213600f2fd1be5550296b7e6b92554b6
Isha Jain <63011793+ish-0709@users.noreply.github.com>
2021-09-18 15:57:39 +0530

    css again

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,15 +7,20 @@ import "./styles.css"
 function Home() {
 	return (
 		<div >
-			<div className="left">
+			<div>
 			<Flipcard />
 			<Flipcard2 />
 			<Flipcard3 />
 			<Flipcard4 />
-			</div>
 			<div className="right">
-
+				<h1>Let's Make Learning Fun!!!</h1>
+				<p>Ever wished for a place where you can ask doubts,download notes,sell older books,
+					do group study with friends etc all at one place. 
+					Don't worry <b>After Class</b> is the right website where you have landed.
+				</p>
 			</div>
+			</div>
+			
 			
 		</div>
 	);
