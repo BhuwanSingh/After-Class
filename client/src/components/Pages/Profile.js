9a842488763f1d2ac49071b77ace755079ebea43
guptabhumika-12 <68741670+guptabhumika-12@users.noreply.github.com>
2021-09-18 19:59:33 +0530

    profilepage added

---
+++ client/src/components/Pages/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    image
+                </div>
+                <div>
+                    <div><h3>Name</h3></div>
+                    <div><h3>Email</h3></div>
+                    <div><h3>Name</h3></div>
+                    <div><h3>Name</h3></div>
+                    <div><h3>Name</h3></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
