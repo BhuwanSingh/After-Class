208c35a552bb15a8c67df605c2300fe1877fee2f
guptabhumika-12 <68741670+guptabhumika-12@users.noreply.github.com>
2021-09-19 16:01:41 +0530

    css added

--- client/src/components/buysell/bookpost/BookPost.js
+++ client/src/components/buysell/bookpost/BookPost.js
@@ -1,40 +1,37 @@
-import { Link } from "react-router-dom";
-import "./bookpost.css";
+import { Link } from 'react-router-dom';
+import './bookpost.css';
 
-export default function Post({img,title,description,price,postedAt,category,contact}) {
-  return (
-    <div className="post">
-      <img
-        className="postImg"
-        src={img}
-        alt=""
-      />
-      <div className="postInfo">
-        <div className="postCats">
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-        {category}
-            </Link>
-          </span>
-       
-        </div>
-        <span className="postTitle">
-          <Link to="/post/abc" className="link">
-          {title}
-          </Link>
-        </span>
-        <hr />
-        <span className="postDate">{postedAt}</span>
-      </div>
-      <p className="postDesc">
-       {description}
-      </p>
-       <p className="postDesc">Contact :  
-        {contact}
-      </p>
-        <p className="postDesc">
-        {price}
-      </p>
-    </div>
-  );
+export default function Post({
+	img,
+	title,
+	description,
+	price,
+	postedAt,
+	category,
+	contact,
+}) {
+	return (
+		<div className='post'>
+			<img className='postImg' src={img} alt='' />
+			<div className='postInfo'>
+				<div className='postCats'>
+					<span className='postCat'>
+						<Link className='link' to='/posts?cat=Music'>
+							{category}
+						</Link>
+					</span>
+				</div>
+				<span className='postTitle'>
+					<Link to='#' className='link'>
+						{title}
+					</Link>
+				</span>
+				<hr />
+				<span className='postDate'>{postedAt}</span>
+			</div>
+			<p className='postDesc'>{description}</p>
+			<p className='postCon'>Contact :{contact}</p>
+			<p className='postprice'>{price}</p>
+		</div>
+	);
 }
