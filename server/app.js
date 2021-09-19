270467db39a9f575664a23b63fbe8aca32a926e3
Shrishesh Pratik <shrisheshpratik@gmail.com>
2021-09-19 07:01:04 -0400

    server hosted

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use("/images", express.static(path.join(__dirname, "./images")));
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
 
