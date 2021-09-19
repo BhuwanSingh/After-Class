5b9c03a99692a1d409384cf4d7fdcf47a7f64c45
BhuwanSingh <bhuwankumar2000@gmail.com>
2021-09-19 13:25:13 +0530

    links removed

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -12,9 +12,9 @@ export default function Post({ post }) {
             <span className="postCat">{c.name}</span>
           ))}
         </div>
-        <Link to={`/post/${post._id}`} className="link">
+        <span to={`/post/${post._id}`} className="link">
           <span className="postTitle">{post.title}</span>
-        </Link>
+        </span>
         <hr />
         <span className="postDate">
           {new Date(post.createdAt).toDateString()}
